8f39d956504d572bb1d4b81958c27ca285a61bdb
Davide Polizzi <davide.polizzi@gmail.com>
2021-01-29 20:30:06 +0300

    initial

---
+++ # To add a new cell, type '# %%'.py
@@ -0,0 +1,20 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# # this is just a hello world test
+
+# %%
+import pandas as pd 
+import numpy as np 
+
+x = pd.read_csv('Test.csv')
+
+
+# %%
+print(x.describe)
+
+
+# %%
+
+
+
---
+++ HelloWorld.py
@@ -0,0 +1,19 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# # This just a Hello world test
+
+# %%
+import pandas as pd 
+import numpy as np 
+
+x = pd.read_csv('Test.csv')
+
+
+# %%
+print(x[1])
+
+
+# %%
+
+
